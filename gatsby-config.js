4f4493fbd769d7777b0f59d5cdbee10803335e24
Benjamin Liet <benjamin.liet.dev@gmail.com>
2023-12-22 09:18:58 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Emily Jones portfolio`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
